e3c141828479384b4c56dd098f535cc4622e6e4a
Rishon73 <sshiff@gmail.com>
2017-07-27 13:49:03 -0400

    Tweaks for Mac
    Actually specifically for my Mac

--- mc_run.sh
+++ mc_run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-SERVER_IP=$(grep -i 'DockerServer' /etc/hosts | grep -v "127" | grep -v "#" | awk '{print $1}')
+#SERVER_IP=$(grep -i 'DockerServer' /etc/hosts | grep -v "127" | grep -v "#" | awk '{print $1}')
+SERVER_IP=192.168.9.1
+HOST_NAME = $(hostname)
 
 if [ "$1" != "" ]; then
 	NAME=$1
 else
 	NAME="mc"
 fi
-
 
 if [ "$2" != "" ]; then
 	TAG=$2
@@ -16,4 +17,4 @@ else
 fi
 
 ##To start the MC container:
-docker run --name $NAME --hostname mc.aos.com --add-host DockerServer:$SERVER_IP --net demo-net -p 8094:8080 --shm-size=2g -d admpresales/mc:$TAG
+docker run --name $NAME --hostname mc.aos.com --add-host HOST_NAME:$SERVER_IP --net demo-net -p 8094:8080 --shm-size=2g -d admpresales/mc:$TAG
