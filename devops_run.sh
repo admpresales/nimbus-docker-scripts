7d36f9aba95812b51b70c38cdcb47bad27dbc904
Rishon73 <sshiff@gmail.com>
2017-10-19 10:38:27 -0700

    comply to NimbusServer

--- devops_run.sh
+++ devops_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SERVER_IP=$(grep -i 'DockerServer' /etc/hosts | grep -v "127" | grep -v "#" | awk '{print $1}')
-CLIENT_IP=$(grep -i 'DockerClient' /etc/hosts | grep -v "127" | grep -v "#" | awk '{print $1}')
+SERVER_IP=$(grep -i 'NimbusServer' /etc/hosts | grep -v "127" | grep -v "#" | awk '{print $1}')
+CLIENT_IP=$(grep -i 'NimbusClient' /etc/hosts | grep -v "127" | grep -v "#" | awk '{print $1}')
 
 if [ "$1" != "" ]; then
 	NAME=$1
@@ -17,6 +17,4 @@ else
 fi
 
 
-docker run -d -p 8090:8080 -p 50000:50000 -p 9022:22 --name $NAME --hostname devops.aos.com --net demo-net \
---add-host DockerServer.aos.com:$SERVER_IP --add-host DockerServer:$SERVER_IP --add-host DockerClient:$CLIENT_IP \
-admpresales/devops:$TAG
+docker run -d -p 8090:8080 -p 50000:50000 -p 9022:22 --name $NAME --hostname devops.aos.com --net demo-net admpresales/devops:$TAG
