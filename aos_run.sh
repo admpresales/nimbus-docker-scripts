7d494421d7046a6283205ca15d2c0a13d66b2925
Rishon73 <sshiff@gmail.com>
2017-11-03 18:11:30 -0400

    use only 1 parameter - the tag

--- aos_run.sh
+++ aos_run.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-if [ "$1" != "" ]; then
-	NAME=$1
-else
-	NAME="aos_main"
-fi
 
-if [ "$2" != "" ]; then
-	TAG=$2
+if [ "$1" != "" ]; then
+	TAG=$1
 else
 	TAG="latest"
 fi
@@ -19,4 +14,4 @@ echo "2. Account Service..."
 docker run -p 8001:8080 --name aos_accountservice --hostname aosaccount.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver' -e "ACCOUNT_IP=nimbusserver" -e "PGPASSWORD=admin" --net demo-net -d admpresales/aos-accountservice:$TAG
 
 echo "3. Main Application..."
-docker run -p 8000:8080 --name $NAME --hostname aosweb.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver' -e "ACCOUNT_IP=nimbusserver" -e "PGPASSWORD=admin" --net demo-net -d admpresales/aos-main-app:$TAG
+docker run -p 8000:8080 --name aos_main --hostname aosweb.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver' -e "ACCOUNT_IP=nimbusserver" -e "PGPASSWORD=admin" --net demo-net -d admpresales/aos-main-app:$TAG
