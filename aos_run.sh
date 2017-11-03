ecacbd8dcdd6e6cdf42da5614cf32677e8691f41
Rishon73 <sshiff@gmail.com>
2017-11-03 18:00:26 -0400

    update aos_run.sh to work with the new 3 containers

--- aos_run.sh
+++ aos_run.sh
@@ -1,20 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-#  aos.sh
-#  
-#
-#  Created by Shahar Shiff on 3/9/17.
-#
+if [ "$1" != "" ]; then
+	NAME=$1
+else
+	NAME="aos_main"
+fi
 
-# Postgres DB
-docker run -d -p 5432:5432 --name aos_postgres --hostname aosdb.aos.com --net demo-net admpresales/aos:postgres
+if [ "$2" != "" ]; then
+	TAG=$2
+else
+	TAG="latest"
+fi
 
-# Web and a[plication server
-docker run -d -p 8000:8000 -p 8009:8009 --net demo-net --name aos_tomcat --hostname aosweb.aos.com --add-host DockerServer:192.168.9.196 admpresales/aos:tomcat
+echo "1. Database..."
+docker run -p 5432:5432 --name aos_postgres --hostname aosdb.aos.com --net -d demo-net admpresales/aos-postgres:$TAG
 
+echo "2. Account Service..."
+docker run -p 8001:8080 --name aos_accountservice --hostname aosaccount.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver' -e "ACCOUNT_IP=nimbusserver" -e "PGPASSWORD=admin" --net demo-net -d admpresales/aos-accountservice:$TAG
 
-# If you want to login to the database and get into psql, login to the image (docker exec -it postgres /bin/bash) and then login as the postgres user (su postgres) with password 
-# mercury123.
-
-
-#exit;
+echo "3. Main Application..."
+docker run -p 8000:8080 --name $NAME --hostname aosweb.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver' -e "ACCOUNT_IP=nimbusserver" -e "PGPASSWORD=admin" --net demo-net -d admpresales/aos-main-app:$TAG
