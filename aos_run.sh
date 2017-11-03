ca49ea00c1f1c61de40ec9cf3b0318864f008593
Rishon73 <sshiff@gmail.com>
2017-11-03 18:15:48 -0400

    fixed '-d' in the wrong place

--- aos_run.sh
+++ aos_run.sh
@@ -8,7 +8,7 @@ else
 fi
 
 echo "1. Database..."
-docker run -p 5432:5432 --name aos_postgres --hostname aosdb.aos.com --net -d demo-net admpresales/aos-postgres:$TAG
+docker run -p 5432:5432 --name aos_postgres --hostname aosdb.aos.com --net demo-net -d admpresales/aos-postgres:$TAG
 
 echo "2. Account Service..."
 docker run -p 8001:8080 --name aos_accountservice --hostname aosaccount.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver' -e "ACCOUNT_IP=nimbusserver" -e "PGPASSWORD=admin" --net demo-net -d admpresales/aos-accountservice:$TAG
