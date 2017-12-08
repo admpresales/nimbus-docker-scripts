8ac5e0b3dfdf1eafacaebdf7c5c63d636dd850e8
Rishon73 <sshiff@gmail.com>
2017-12-08 08:47:08 -0800

    update run command

--- alm_run.sh
+++ alm_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SERVER_IP=$(grep -i 'DockerServer' /etc/hosts | grep -v "127" | grep -v "#" | awk '{print $1}')
+SERVER_IP=$(grep -i 'NimbusServer' /etc/hosts | grep -v "127" | grep -v "#" | awk '{print $1}')
 if [ "$1" != "" ]; then
 	NAME=$1
 else
@@ -15,5 +15,5 @@ else
 fi
 
 
-docker run --name $NAME --hostname alm.aos.com --net demo-net -p 8082:8080 --shm-size=2g -d \
---add-host DockerServer:$SERVER_IP admpresales/alm:$TAG
+#docker run --name $NAME --hostname alm.aos.com --net demo-net -p 8082:8080 --shm-size=2g -d --add-host DockerServer:$SERVER_IP admpresales/alm:$TAG
+docker run -d -p 8082:8080 -p 1521:1521 --name $NAME --hostname alm.aos.com --net demo-net --shm-size=2g admpresales/alm:$TAG
