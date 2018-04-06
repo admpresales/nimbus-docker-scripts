37a5be4c7a0d4431fc712a90559a3f92d76c43d0
Rishon73 <sshiff@gmail.com>
2018-04-06 14:26:16 -0700

    some updates

--- aos_run.sh
+++ aos_run.sh
@@ -8,10 +8,15 @@ else
 fi
 
 echo "1. Database..."
-docker run -p 5432:5432 --name aos_postgres --hostname aosdb.aos.com --net demo-net -d admpresales/aos-postgres:$TAG
+docker run -d -p 5432:5432 --name aos_postgres --hostname aosdb.aos.com --net demo-net admpresales/aos-postgres:$TAG
 
 echo "2. Account Service..."
-docker run -p 8001:8080 --name aos_accountservice --hostname aosaccount.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver' -e "ACCOUNT_IP=nimbusserver" -e "PGPASSWORD=admin" --net demo-net -d admpresales/aos-accountservice:$TAG
+docker run -p 8001:8080 --name aos_accountservice --hostname aosaccount.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" \
+-e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver' -e "ACCOUNT_IP=nimbusserver" -e "PGPASSWORD=admin" \
+--net demo-net -d admpresales/aos-accountservice:$TAG
 
 echo "3. Main Application..."
-docker run -p 8000:8080 --name aos_main --hostname aosweb.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver' -e "ACCOUNT_IP=nimbusserver" -e "PGPASSWORD=admin" --net demo-net -d admpresales/aos-main-app:$TAG
+docker run -p 8000:8080 --name aos_main --hostname aosweb.aos.com -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" \
+-e "MAIN_PORT=8000" -e "ACCOUNT_PORT=8001" -e "MAIN_IP=nimbusserver" -e "ACCOUNT_IP=nimbusserver" -e "PGPASSWORD=admin" \
+-e "http_proxy=${http_proxy}" -e "https_proxy=${https_proxy}" --add-host nimbusserver:172.50.0.1 --net demo-net \
+-d admpresales/aos-main-app:$TAG
