6c3aa06561df9463c6d22b7d128d8459bc0d65eb
demo <demo@mc.aos.com>
2018-06-18 16:04:37 -0700

    using nimbusserver.aos.com to allow using srf tunnel

--- aos_run.sh
+++ aos_run.sh
@@ -14,12 +14,13 @@ docker run -d -p 5432:5432 --name aos_postgres \
 echo "2. Account Service..."
 docker run -p 8001:8080 --name aos_accountservice --hostname aosaccount.aos.com \
 -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" \
--e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver' -e "ACCOUNT_IP=nimbusserver" \
+-e "ACCOUNT_PORT=8001" -e 'MAIN_IP=nimbusserver.aos.com' -e "ACCOUNT_IP=nimbusserver.aos.com" \
 -e "PGPASSWORD=admin" --net demo-net -d admpresales/aos-accountservice:$TAG
 
 echo "3. Main Application..."
 docker run -p 8000:8080 --name aos_main --hostname aosweb.aos.com \
 -e "POSTGRES_PORT=5432" -e "POSTGRES_IP=aos_postgres" -e "MAIN_PORT=8000" \
--e "ACCOUNT_PORT=8001" -e "MAIN_IP=nimbusserver" -e "ACCOUNT_IP=nimbusserver" \
+-e "ACCOUNT_PORT=8001" -e "MAIN_IP=nimbusserver.aos.com" -e "ACCOUNT_IP=nimbusserver.aos.com" \
 -e "PGPASSWORD=admin" -e "http_proxy=${http_proxy}" -e "https_proxy=${https_proxy}" \
---add-host nimbusserver:172.50.0.1 --net demo-net -d admpresales/aos-main-app:$TAG
+--add-host nimbusserver:172.50.0.1 --add-host nimbusserver.aos.com:172.50.0.1 \
+--net demo-net -d admpresales/aos-main-app:$TAG
