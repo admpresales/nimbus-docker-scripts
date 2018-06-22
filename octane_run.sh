e087cb2b456d15c62c074fb422649ca6704e7140
Rishon73 <shahar.shiff@hpe.com>
2018-06-22 09:28:02 -0700

    removed comment

--- octane_run.sh
+++ octane_run.sh
@@ -15,8 +15,5 @@ else
 	TAG="latest"
 fi
 
-#docker run --name $NAME --hostname octane.aos.com --net demo-net -p 8085:8080 --add-host DockerServer:$SERVER_IP --add-host DockerServer.aos.com:$SERVER_IP \
-#--shm-size=2g -p 9081:9081 -p 9082:9082 -p 1099:1099 -d admpresales/octane:$TAG
-
 docker run -d -p 1099:1099 -p 8085:8080 -p 9081:9081 -p 9082:9082 --name $NAME --hostname octane.aos.com --net demo-net \
- -e OCTANE_HOST=nimbusserver.aos.com --shm-size=2g admpresales/octane:$TAG
+-e OCTANE_HOST=nimbusserver.aos.com --shm-size=2g admpresales/octane:$TAG
