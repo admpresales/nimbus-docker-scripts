2e8f0d533735ca7beb253303800b311cf4070128
Rishon73 <sshiff@gmail.com>
2018-10-02 13:42:38 -0700

    removing un-needed file

--- start_srl_agent.sh
+++
@@ -1,6 +0,0 @@
-
-docker run -t -i \
--e "STORM_DISABLE_AUTO_REGISTER=1" \
---net=host performancetesting/stormrunner_load_generator
-
-# The current working folder is: /opt/testexec.server/stormagent
