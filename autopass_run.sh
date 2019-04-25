76ee3af8394cff2b64571e2a534801cc849c2987
Jason Hrabi <jason.hrabi@microfocus.com>
2019-04-25 14:55:12 -0400

    Updated gitignore for intellij. Removed non-nimbusapp config files.

--- autopass_run.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-
-docker run -d --hostname autopass.aos.com --ip=172.50.10.10 --name autopass --net demo-net -p 5814:5814 --restart=always admpresales/autopass:10.7.0_d
-
-#docker run --name autopass --hostname autopass.aos.com --net demo-net -p 5814:5814 --ip=172.50.10.10 --restart=always -d admpresales/autopass:9.3_d
-
-#docker run --name autopass --hostname autopass.aos.com --net demo-net -p 5814:5814 --ip=172.50.10.10 --restart=always -d hub.docker.hpecorp.net/hpepresales/autopass:9.3_d
--- srl_lg_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -t -i \
-      -e "STORM_TENANT=159221713"  \
-      -e "STORM_USERNAME=shahar.shiff@hpe.com" \
-      -e "STORM_PASSWORD=Dagnahash3#" \
-      --net=host performancetesting/stormrunner_load_generator:latest
-
-
