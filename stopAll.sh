5f229ba4f84b432a45ccd86a834cafb9dd4d8bf8
Rishon73 <sshiff@gmail.com>
2019-04-25 14:40:19 -0400

    cleanup, adding all possible containers to stop  and addedsome  comment

--- stopAll.sh
+++ stopAll.sh
@@ -3,6 +3,7 @@
 # This script uses nimbusadmin to stop running containers at once.
 # It will not stop autopass
 
+# for special case (aos actully kick starts 3 containers)
 STOP_AOS=0
 
 function stopContainer()
@@ -21,7 +22,7 @@ docker ps --format "table {{.Names}}" | awk '{print $1}' | while read container;
     fi
     continue
     ;;
-    devops* | octane* | mc* | alm* | intellij* | nv*)
+    devops* | octane* | mc* | alm* | intellij* | nv* | ppm* | da* | ssc* | jira* | sv* | agm*)
     stopContainer
     ;;
     NAMES)
@@ -33,38 +34,9 @@ docker ps --format "table {{.Names}}" | awk '{print $1}' | while read container;
     continue
     ;;
     *)
-    echo "Unknown container '$container'"
+    echo "Unknown container '$container', skipping..."
     continue
     ;;
   esac
 sleep 2
 done
-
-
-exit 0
-
-  if [ "$container" != "NAMES" ]; then
-    if [ "$container" = "aos_main" ] || [ "$container" = "aos_postgres" ] || [ "$container" = "aos_accountservice" ]; then
-      container="aos"
-      echo "2..$container..$STOP_AOS.."
-    fi
-    if [ "$container" != "aos" ] ; then
-      echo "nimbusapp $container stop"
-      #nimbusapp ${container} stop
-    elif [ "$STOP_AOS" -eq "0" ]; then
-      echo "nimbusapp aos stop"
-      #nimbusapp aos stop
-      STOP_AOS=1
-    fi
-  fi
-
-
-
-#echo "nimbusapp octane stop"
-#nimbusapp octane stop
-#echo "nimbusapp devops stop"
-#nimbusapp devops stop
-#echo "nimbusapp aos stop"
-#nimbusapp aos stop
-#echo "nimbusapp intellij stop"
-#nimbusapp intellij stop
